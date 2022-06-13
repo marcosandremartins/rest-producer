7d16f7444e47947d1af5b6b0484828f261c6588a
Marcos Martins <marcos.martins@farfetch.com>
2022-06-13 16:38:04 +0100

    build: change kafka default port on entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i 's|localhost:9092|'"$KAFKA_BROKERS"'|g' appsettings.json
+sed -i 's|localhost:9094|'"$KAFKA_BROKERS"'|g' appsettings.json
 sed -i 's|localhost:8085|'"$SCHEMA_REGISTRY_URL"'|g' appsettings.json
 dotnet KafkaRestProducer.dll
